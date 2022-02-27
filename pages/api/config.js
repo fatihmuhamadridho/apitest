57fcb084ddf97a3311e58f0dfb2c5024d3a4fe9f
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2022-02-27 11:29:24 +0700

    env

--- pages/api/config.js
+++ pages/api/config.js
@@ -1,7 +1,19 @@
 const admin = require('firebase-admin');
 const credential = require('../../ServiceAccountKey.json')
+require('dotenv').config();
 
-admin.initializeApp({ credential: admin.credential.cert(credential)} )
+admin.initializeApp({ credential: admin.credential.cert({
+    type: process.env.type,
+    projectId: process.env.project_id,
+    privateKeyId: process.env.private_key_id,
+    privateKey: process.env.private_key,
+    clientEmail: process.env.client_email,
+    clientId: process.env.client_id,
+    authUri: process.env.auth_uri,
+    tokenUri: process.env.token_uri,
+    authProviderX509CertUrl: process.env.auth_provider_x509_cert_url,
+    clientX509CertUrl: process.env.client_x509_cert_url,
+}) })
 const db = admin.firestore();
 const Users = db.collection("Users");
 
