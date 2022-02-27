f3b5f18f9de4f821cb6f11ded8d8e0882a3917d9
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2022-02-27 10:55:55 +0700

    service

--- pages/api/config.js
+++ pages/api/config.js
@@ -1,5 +1,7 @@
 const admin = require('firebase-admin');
+const credential = require('../../ServiceAccountKey.json')
 
+admin.initializeApp({ credential: admin.credential.cert(credential)} )
 const db = admin.firestore();
 const Users = db.collection("Users");
 
